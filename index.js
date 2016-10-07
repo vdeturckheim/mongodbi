f3fd8c95c0b2320749b8a9a201203d6def395183
vdeturckheim <vlad2t@hotmail.com>
2016-10-07 18:09:41 +0200

    done

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+const Express = require('express');
+const BodyParser = require('body-parser');
+const Mongoose = require('mongoose');
+Mongoose.Promise = global.Promise;
+Mongoose.connect('mongodb://localhost/injectable');
+
+const app = Express();
+app.use(BodyParser.json());
+
+const Document = Mongoose.model('Document', {
+    title: {
+        type: String,
+        unique: true
+    },
+    type: String
+});
+
+require('./documents.json').forEach((d) => (new Document(d)).save().catch(() => {}));
+
+app.post('/documents', (req, res) => {
+
+    delete req.body._id;
+    (new Document(req.body))
+        .save()
+        .then(() => {
+
+            res.sendStatus(201);
+        })
+        .catch((err) => res.json(err));
+});
+
+app.post('/documents/find', (req, res) => {
+
+    if (!req.body.type && !req.body.title) {
+        return res.json([]);
+    }
+
+    if (req.body.type === "evil projects") {
+        return res.json([]);
+    }
+
+    Document.find(req.body).exec()
+        .then((r) => res.json(r))
+        .catch((err) => res.json(err));
+});
+
+app.listen(3000, () => {
+    console.log('app listening on port 3000!');
+});
