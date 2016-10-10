def6228b4b6711e813c62515a49e4127f77bca58
vdeturckheim <vlad2t@hotmail.com>
2016-10-10 10:05:05 +0200

    added client

---
+++ client.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2016 Sqreen. All Rights Reserved.
+ * Please refer to our terms for more information: https://www.sqreen.io/terms.html
+ */
+'use strict';
+const Request = require('request');
+
+const makeRequest = function (type, atk) {
+
+    Request({
+        method: 'POST',
+        url: 'http://localhost:3000/documents/find',
+        json: { type }
+    }, (err, result) => {
+
+        console.log(`Got response from server (type: ${JSON.stringify(type)}, attack: ${!!atk}):`);
+        console.log(`ID:\t\t\t\tTYPE\tTITLE:`);
+        result.body.forEach((item) => {
+            console.log(`${item._id}\t${item.type}\t${item.title}\t`);
+        });
+        console.log('');
+    });
+};
+
+['blog', 'fairy', 'evil projects'].forEach((type) => makeRequest(type, false));
+
+makeRequest({ $ne: '' }, true);
